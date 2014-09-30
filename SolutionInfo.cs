6ae97dc4531947e64b6c49c7e737ddc6f25279f9
shearer3000 <shearer3000@gmail.com>
2014-10-01 11:17:41 +1300

    solution versioning

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -5,6 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("visualCaptcha-Nuget")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Visual captcha .net port")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
